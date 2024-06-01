8104470554e181e1f4ae77fe09e57c267755421f
Umair Khan <umair@Umairs-MacBook-Pro.local>
2024-05-31 23:07:38 -0400

    updated movement_distance

--- mouse_jiggler/jiggler.py
+++ mouse_jiggler/jiggler.py
@@ -10,7 +10,7 @@ jiggler_running = False
 
 def executable():
 
-    def mouse_jiggler_function(movement_distance=5, interval=0.5):
+    def mouse_jiggler_function(movement_distance=10, interval=0.5):
         global jiggler_running
         screen_width, screen_height = pyautogui.size()
 
